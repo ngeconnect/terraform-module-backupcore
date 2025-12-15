751661f63b76c51ab7c005ee05d831940d952a83
Dielvic KOMBILA <dkombila@nge.fr>
2025-12-15 15:41:13 +0100

    fix

--- variable.tf
+++ variable.tf
@@ -33,3 +33,7 @@ variable "clustername" {
   description = "Clustername"
 }
 
+variable "environment" {
+  type        = string
+  description = "Environment name used to suffix backup storage account"
+}
