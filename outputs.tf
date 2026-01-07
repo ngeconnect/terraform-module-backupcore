97a88c31253900eb1b67fee1a22a210d4c823f14
Dielvic KOMBILA <dkombila@nge.fr>
2026-01-07 17:12:17 +0100

    Add Outputs

---
+++ outputs.tf
@@ -0,0 +1,22 @@
+# Fichier: outputs.tf (à la racine du projet Core)
+
+output "backup_vault_id" {
+  value = module.backup_core.backup_vault_id
+}
+
+output "backup_vault_principal_id" {
+  # Vérifie que le module exporte bien ça, sinon adapte le nom
+  value = module.backup_core.backup_vault_principal_id 
+}
+
+output "backup_policy_id" {
+  value = module.backup_core.backup_policy_id
+}
+
+output "backup_sa_name" {
+  value = module.backup_core.backup_sa_name
+}
+
+output "backup_container_name" {
+  value = module.backup_core.backup_container_name
+}
